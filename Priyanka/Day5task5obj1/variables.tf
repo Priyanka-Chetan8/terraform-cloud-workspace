1c98d731a614f80e0e0354a7ce7a79bab059762f
DEVOPS <priyachetan0229@gmail.com>
2025-06-17 13:17:55 +0530

    Obj1 define and use variables in main.tf file

---
+++ Priyanka/Day5task5obj1/variables.tf
@@ -0,0 +1,8 @@
+variable "ami_id" {
+  description = "This variable is used for ami id definition."
+}
+
+variable "instance_type" {
+  description = "The type of instance to use for the EC2 instance."
+  type        = string
+}
