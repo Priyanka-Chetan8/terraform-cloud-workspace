6ca96a4edd6d747549a60879aa1e83a91359334a
DEVOPS <priyachetan0229@gmail.com>
2025-06-17 16:28:19 +0530

    If we dont provide ami id in terraform.tfvars and since I have declared in main.tf file, it will ask for ami id during terraform plan

---
+++ Priyanka/Day5task5obj3/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID used to launch an EC2 instance"
+  type        = string
+}
